512aeb2426ba43d91503aa068f3f5bade41bbc39
David Parsley <parsley@linuxjedi.org>
2021-08-31 18:39:36 +0000

    Create an instance

--- modules/gopherbot-ec2/bot-ec2.tf
+++ modules/gopherbot-ec2/bot-ec2.tf
@@ -1,51 +1,16 @@
-data "aws_region" "current" {}
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners = ["amazon"]
 
-data "aws_caller_identity" "current" {}
-
-resource "aws_iam_instance_profile" "bot_profile" {
-  name = "bot_profile"
-  role = aws_iam_role.bot_role.name
-}
-
-resource "aws_iam_role" "bot_role" {
-  name = "bot_role"
-  path = "/"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
-    "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-  ]
-  inline_policy {
-    name = "param-access-policy"
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Effect   = "Allow",
-          Action   = ["ssm:DescribeParameters"]
-          Resource = "*"
-        },
-        {
-          Effect   = "Allow"
-          Action   = ["ssm:GetParameters"]
-          Resource = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/robots/${var.robot-name}/*"
-        }
-      ]
-    })
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
   }
+}
+
+resource "aws_instance" "robot" {
+  ami                  = data.aws_ami.amazon-linux-2.id
+  instance_type        = var.instance-type
+  subnet_id            = var.subnet-id
+  iam_instance_profile = aws_iam_instance_profile.bot_profile.name
 }
