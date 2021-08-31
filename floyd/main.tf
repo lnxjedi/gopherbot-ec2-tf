59173fc000a0e4f62b151b45c3aa586b619de6a7
David Parsley <parsley@linuxjedi.org>
2021-08-31 19:38:08 +0000

    Add subnet id

--- floyd/main.tf
+++ floyd/main.tf
@@ -6,10 +6,18 @@ variable "deploy-key" {
   type = string
 }
 
+data "aws_subnet" "floydnet" {
+  filter {
+    name   = "tag:Name"
+    values = ["Management DMZ Subnet A"]
+  }
+}
+
 module "floyd-gopherbot" {
   source         = "../modules/gopherbot-ec2"
   robot-name     = "floyd"
   encryption-key = var.encryption-key
   repository     = "git@github.com:parsley42/floyd-gopherbot.git"
   deploy-key     = var.deploy-key
+  subnet-id      = data.aws_subnet.floydnet.id
 }
