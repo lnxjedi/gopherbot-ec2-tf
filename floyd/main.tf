2af4c8e92933b230032cea0323039874e2cc13aa
David Parsley <david@welldhealth.com>
2021-08-31 08:48:37 -0400

    Fix dir name

--- floyd/main.tf
+++ floyd/main.tf
@@ -1,5 +1,5 @@
 module "floyd-gopherbot" {
-  source         = "../modules/gopherbot-ec2-tf"
+  source         = "../modules/gopherbot-ec2"
   encryption-key = local.encryption-key
   repository     = "git@github.com:parsley42/floyd-gopherbot.git"
   protocol       = "slack"
