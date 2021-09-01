eb0ce9c93197c639157361313387620f61a2a77e
David Parsley <parsley@linuxjedi.org>
2021-09-01 18:35:38 +0000

    Revert to launch template

--- modules/gopherbot-ec2/bot-ec2.tf
+++ modules/gopherbot-ec2/bot-ec2.tf
@@ -8,15 +8,28 @@ data "aws_ami" "amazon-linux-2" {
   }
 }
 
-resource "aws_launch_configuration" "bot-template" {
-  name          = "robot-template"
-  image_id      = data.aws_ami.amazon-linux-2.id
-  instance_type = var.instance-type
-  user_data     = filebase64("${path.module}/bootstrap.sh")
+resource "aws_launch_template" "bot-template" {
+  name                                 = "robot-template"
+  image_id                             = data.aws_ami.amazon-linux-2.id
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type                        = var.instance-type
+  update_default_version               = true
+  user_data = filebase64("${path.module}/bootstrap.sh")
 
-  iam_instance_profile        = aws_iam_instance_profile.bot_profile.name
-  enable_monitoring           = false
-  associate_public_ip_address = true
+  iam_instance_profile {
+    name = aws_iam_instance_profile.bot_profile.name
+  }
+  monitoring {
+    enabled = false
+  }
+  network_interfaces {
+    associate_public_ip_address = true
+    subnet_id                   = var.subnet-id
+  }
+  tag_specifications {
+    resource_type = "instance"
+    tags          = var.tags
+  }
 }
 
 resource "aws_autoscaling_group" "immortal-bot" {
@@ -35,5 +48,7 @@ resource "aws_autoscaling_group" "immortal-bot" {
     value = "${var.robot-name}-robot"
     propagate_at_launch = true
   }
-  launch_configuration = aws_launch_configuration.bot-template.name
+  launch_template {
+    name = aws_launch_template.bot-template.name
+  }
 }
